df5d1d94b5b754d966dc0213b0a9dfcfc670d292
bibongbenh04 <116509194+bibongbenh04@users.noreply.github.com>
2025-05-07 02:18:21 +0700

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^m=4)jz@+4kqfy8mryo)s*a%)i60p1j1tu*(jd66rm9_#q9o05
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 CACHES = {'default': {'BACKEND': 'django.core.cache.backends.dummy.DummyCache', 'LOCATION': ''}}
-CSRF_TRUSTED_ORIGINS = ['https://sfotipy-bua9bcenahh7fpbe.uksouth-01.azurewebsites.net']
+CSRF_TRUSTED_ORIGINS = ['https://sfotipy-bua9bcenahh7fpbe.uksouth-01.azurewebsites.net','https://cnpm-production-739b.up.railway.app']
 
 from django.utils.translation import gettext_lazy as _
 LANGUAGE = [
